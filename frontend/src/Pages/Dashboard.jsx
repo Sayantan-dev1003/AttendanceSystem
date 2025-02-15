c2738c18f90222f500dbf728536fd8ef3aec23a1
Sayantan Halder <sayantanhalder78@gmail.com>
2025-02-15 20:04:34 +0530

    enhanced signin/signup features

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,6 +1,19 @@
+import axios from 'axios';
+
 const Dashboard = () => {
+  const handleLogout = async () => {
+    try {
+      await axios.post('/api/logout');
+      window.location.href = '/'; // Redirect to the home page after logout
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
+
   return (
-    <div>Dashboard</div>
+    <>
+      <button className="bg-red-500 text-white py-2 px-4 rounded-lg cursor-pointer" onClick={handleLogout}>Logout</button>
+    </>
   )
 }
 
